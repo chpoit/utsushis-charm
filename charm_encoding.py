e0a173900a0792412f1ce529d3ced9cb25ba4c5b
Charles Poitras <charlesp95@gmail.com>
2021-05-09 00:16:33 -0400

    Simple encode (#4)
    * Updated to use super simple encoding
    
    * Calling encoder in main.py

---
+++ charm_encoding.py
@@ -0,0 +1,14 @@
+import json
+from Charm import Charm 
+
+
+def encode_charms(charm_file):
+    with open(charm_file) as cjsf:  
+        charms = json.load(cjsf)
+    with open("charms.encoded.txt", "w") as encoded:
+        for charm in map(Charm.from_dict, charms):
+            encoded.write(f"{charm.to_simple_encode()}\n")
+
+if __name__ == '__main__':
+    charm_file = "charms.json"
+    encode_charms(charm_file)
