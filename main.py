6b674f2365e57798168746d19b8b941618285b37
Charles Poitras <charlesp95@gmail.com>
2021-05-14 16:27:08 -0400

    cleaned up main

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from src.__main__ import main
-import os
-import sys
 from src.arg_builder import build_args
-import multiprocessing
 
 if __name__ == "__main__":
-    multiprocessing.freeze_support()
     args = build_args()
     main(args)
 
