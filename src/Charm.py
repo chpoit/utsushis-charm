100b0db3a3c34fdbc7cf847062d585064330a84c
Charles Poitras <charlesp95@gmail.com>
2021-05-26 16:23:59 -0400

    fixed charm compare bug

--- src/Charm.py
+++ src/Charm.py
@@ -6,7 +6,7 @@ class Charm:
     def __init__(self, slots, skills=None):
         if not skills:
             skills = {}
-        self.slots = slots
+        self.slots = list(sorted(slots, reverse=True))
         self.skills = skills
 
     def __eq__(self, other):
@@ -29,14 +29,14 @@ class Charm:
 
     @staticmethod
     def from_dict(json_data):
-        return Charm(json_data['slots'], json_data['skills'])
+        return Charm(json_data["slots"], json_data["skills"])
 
     def to_dict(self):
-        return {'slots': self.slots, 'skills': self.skills}
+        return {"slots": self.slots, "skills": self.skills}
 
     def is_identical(self, charm):
-        if self.slots[0] != charm.slots[0] and\
-            self.slots[1] != charm.slots[1] and\
+        if self.slots[0] != charm.slots[0] or\
+            self.slots[1] != charm.slots[1] or\
                 self.slots[2] != charm.slots[2]:
             return False
 
@@ -54,8 +54,10 @@ class Charm:
         acc = ""
         for skill in self.skills:
             acc += f"{skill},{self.skills[skill]},"
-        if len(self.skills) == 1:
-            acc += f",0,"
+        if len(self.skills) == 0:  # should be impossible
+            acc += ",0,"
+        if len(self.skills) <= 1:
+            acc += ",0,"
 
         for level in self.slots:
             acc += f"{level},"
