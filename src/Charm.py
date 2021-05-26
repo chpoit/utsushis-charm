772fd315e32c955ff529dce6aaf17f4c76180ac0
Awajsbro <awajsbro@student.42.fr>
2021-05-26 20:51:09 +0200

    === charm_extraction.py Charm.py === I add the frame name at the charm to find it more easily on my chest
    === skillsRank.json ===
    All skills with a ranking value base on god talisman checker at https://gamewith.net/monsterhunter-rise/article/show/28392
    
    === skillsToJewel.json ===
    All skills we can craft as jewel with the jewel's lvl
    
    === trashSkills.txt ===
    File for write skills name for ignore it during the ranking process if the user really don't care about some skills
    
    === rebirth.py ===
    Get the charms.json make by your utsushis_charm for ranking all charms and give a liste of charm you can rebirth
    Charms for rebirth are charms you can "remake" with another
    ex: you can make any charm with 1 skill at 1 lvl with a 3slot charm
    Also create a json file for the ranked charms and another for the charm to rebirth
    run it with: "python rebirth.py"
    
    === More ===
    I'm not a python dev so sorry if i maybe do some bad stuff
    Feel free to integrete the rebirth.py if you want
    my apologies for my english and thank you a lot for utsushi_charm you save to me a great amount of time =D

--- src/Charm.py
+++ src/Charm.py
@@ -2,11 +2,11 @@ import json
 
 
 class Charm:
-
-    def __init__(self, slots, skills=None):
+    def __init__(self, slots, frame_loc, skills=None):
         if not skills:
             skills = {}
         self.slots = slots
+        self.frame = frame_loc
         self.skills = skills
 
     def __eq__(self, other):
@@ -29,23 +29,24 @@ class Charm:
 
     @staticmethod
     def from_dict(json_data):
-        return Charm(json_data['slots'], json_data['skills'])
+        return Charm(json_data["slots"], json_data["frame"], json_data["skills"])
 
     def to_dict(self):
-        return {'slots': self.slots, 'skills': self.skills}
+        return {"slots": self.slots, "skills": self.skills, "frame": self.frame}
 
     def is_identical(self, charm):
-        if self.slots[0] != charm.slots[0] and\
-            self.slots[1] != charm.slots[1] and\
-                self.slots[2] != charm.slots[2]:
+        if (
+            self.slots[0] != charm.slots[0]
+            and self.slots[1] != charm.slots[1]
+            and self.slots[2] != charm.slots[2]
+        ):
             return False
 
         if len(self.skills) != len(charm.skills):
             return False
 
         for skill in self.skills:
-            if skill not in charm.skills or\
-                    self.skills[skill] != charm.skills[skill]:
+            if skill not in charm.skills or self.skills[skill] != charm.skills[skill]:
                 return False
 
         return True
